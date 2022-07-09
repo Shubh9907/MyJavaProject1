31f45eae997206de4feb2ddf64abe36d96e4d72b
Shubh9907 <Shubhamv60047@gmail.com>
2022-07-09 11:13:24 +0530

    Added: Files

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,37 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        double random =Math.floor(Math.random()*1000 % 10000);
+        System.out.println((int) random);
+
+        0.124688*1000 = 124.688 % 10 = 4
+        entry Contr      Exit Control
+        for                 Do while
+        while
+            int i = 15;
+            while ( true ) {
+                System.out.println(i);
+                if ( i == 100 ){
+                    break;
+                }
+                i++;
+            }
+
+            for(int a = 0; a<= 10; a++ ) {
+                System.out.println(a);
+            }
+
+            do {
+                System.out.println(i);
+                i++;
+                if (i == 10) {
+                    break;
+                }
+            }while (true);
+        System.out.println("hello");
+        System.out.println("World");
+        System.out.println("My name is shubham");
+    }
+}
